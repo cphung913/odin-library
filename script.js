c077e599f5425838e62deb2be666888e80feab97
cphung913 <chasephung13@gmail.com>
2023-07-11 22:19:56 -0700

    setup book create funtion

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 const newButton = document.querySelector(".new");
+const main = document.querySelector(".main")
 
-let myLibrary = [];
+const card = document.createElement("div");
+const title = document.createElement("div");
+const author = document.createElement("div");
+const readStatus = document.createElement("img");
+const editButton = document.createElement("button");
+const deleteButton = document.createElement("button")
 
-newButton.addEventListener("click", makeBook)
+newButton.addEventListener("click", createBook);
 
-function makeBook() {
+readStatus.setAttribute("src", "");
+card.classList.add("card");
+title.classList.add("title");
+author.classList.add("author");
+readStatus.classList.add("read-status");
+editButton.classList.add("edit");
+deleteButton.classList.add("delete");
 
-}
+function createBook() {
+    const t = title.cloneNode();
+    const a = author.cloneNode();
+    const r = readStatus.cloneNode();
+    const c = card.cloneNode();
+    const e = editButton.cloneNode();
+    const d = deleteButton.cloneNode();
 
-function Book(title, author, read) {
-  this.title = title;
-  this.author = author;
-  this.read = read
-}
-
-Book.prototype.addBookToLibrary = function() {
-  myLibrary.push(this);
-}
-
-function displayBooks(array) {
-
+    t.textContent = "Title: " + "me";
+    a.textContent = "Author: " + "me";
+    e.textContent = "Edit";
+    d.textContent = "Delete";
+    
+    c.append(t, a, r, e, d);
+    main.appendChild(c);
 }
